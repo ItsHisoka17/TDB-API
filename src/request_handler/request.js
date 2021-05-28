e065f86dff03c52f777315fe2b18e748dd22c888
HashT9u <61894183+HashT9u@users.noreply.github.com>
2021-05-28 11:28:29 -0400

    Fixed Endpoint in RequestHandler

--- src/request_handler/request.js
+++ src/request_handler/request.js
@@ -6,18 +6,24 @@ async function _request(path, query = {}){
         let regex = /^\/([A-Za-z]{3})\.([A-Za-z]{3})$/g;
         let urlMatch = regex.exec(path);
         if (!urlMatch) reject(new Error('Invalid Path'));
-        let endpoint = urlMatch.slice(1).join('.');
+
+        let endpoint = `/${urlMatch.slice(1).join('.')}`
         let queryString = Object.keys(query).lenght>0?query:false;
         let url;
+
         if (!queryString){
             url = `${baseURL}${endpoint}`
         } else {
             url = `${baseURL}${endpoint}?`
+
         for (const [query, val] of Object.entries(queryString)){
             url += `${query}=${val}&`
         }
+
         let response = await fetch.get(url);
+
         if (!response.ok || response.status !== 200) reject(new Error(`${response.statusText}`))
+        
         return resolve(response.body)
         }
     })
